2db32f8010ce309d9d03a116517ac76ba2eca38d
abhaycollege <abma22ise@cmrit.ac.in>
2025-06-03 06:22:20 +0530

    first commit

---
+++ src/main/java/p8t/p8t/App.java
@@ -0,0 +1,13 @@
+package p8t.p8t;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
